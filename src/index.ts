c1f1673aac46eeb2439fc35f56fdd62c2ef492b9
George Czabania <1140167+stayradiated@users.noreply.github.com>
2019-09-10 19:05:31 +0200

    test: add unit test

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,21 @@ import path from 'path'
 
 const JADX_PATH = path.join(__dirname, '../resources/jadx-1.0.0/bin/jadx')
 
-const jadx = async (sourceDexPath: string, destinationDirectory: string) => {
+export type JadxFn = (
+  sourceDexPath: string,
+  destinationDirectory: string,
+) => Promise<void>
+
+const jadx: JadxFn = async (sourceDexPath, destinationDirectory) => {
   await execa(
     JADX_PATH,
     [
       '--no-res', // do not decode resources
       '--show-bad-code',
-      '--threads-count', '6',
-      '--output-dir', destinationDirectory,
+      '--threads-count',
+      '6',
+      '--output-dir',
+      destinationDirectory,
       sourceDexPath,
     ],
     {
